6fd5a42a04f7f9d300967df217ea6fe9b5f328be
Ryan Gross <ryangross123@icloud.com>
2019-05-08 12:28:15 -0500

    Scaffoled the product model

---
+++ Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Final_Project_Homekit_3.Models;
+
+namespace Final_Project_Homekit_3.Pages.Products
+{
+    public class EditModel : PageModel
+    {
+        private readonly Final_Project_Homekit_3.Models.HomekitDbContext _context;
+
+        public EditModel(Final_Project_Homekit_3.Models.HomekitDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _context.Product
+                .Include(p => p.Brand)
+                .Include(p => p.Category).FirstOrDefaultAsync(m => m.ProductID == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+           ViewData["BrandID"] = new SelectList(_context.Brand, "BrandID", "BrandName");
+           ViewData["CategoryID"] = new SelectList(_context.Category, "CategoryID", "CategoryName");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(Product.ProductID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.ProductID == id);
+        }
+    }
+}
